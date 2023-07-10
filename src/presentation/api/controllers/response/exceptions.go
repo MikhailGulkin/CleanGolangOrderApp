c36c2e7cace77bf7e6ee4b58db1e24468de637fd
Bol4onok <what@about.com>
2023-07-10 16:53:04 +0300

    refactoring excp

--- src/presentation/api/controllers/response/exceptions.go
+++ src/presentation/api/controllers/response/exceptions.go
@@ -1,6 +1,13 @@
 package response
 
+import "github.com/MikhailGulkin/simpleGoOrderApp/src/domain"
+
 type ExceptionResponse struct {
 	Message string `json:"message"`
 	Data    string `json:"data"`
+}
+
+func SetExceptionPayload(response *ExceptionResponse, exc domain.CustomException) {
+	response.Message = exc.Message
+	response.Data = exc.Ctx
 }
--- src/presentation/api/middleware/errorHandler/product.go
+++ src/presentation/api/middleware/errorHandler/product.go
@@ -4,8 +4,14 @@ import (
 	"errors"
 	application "github.com/MikhailGulkin/simpleGoOrderApp/src/application/order/exceptions"
 	domain "github.com/MikhailGulkin/simpleGoOrderApp/src/domain/entities"
+	"github.com/MikhailGulkin/simpleGoOrderApp/src/presentation/api/controllers/response"
 	"net/http"
 )
+
+type ErrorStatus struct {
+	status    int
+	exception any
+}
 
 func handleProductError(e ErrorCatching) {
 	var discountError *domain.InvalidDiscountProductCreation
@@ -15,22 +21,18 @@ func handleProductError(e ErrorCatching) {
 
 	if errors.As(e.err, &discountError) {
 		*e.status = http.StatusBadRequest
-		e.exception.Message = discountError.Message
-		e.exception.Data = discountError.Ctx
+		response.SetExceptionPayload(e.exception, discountError.CustomException)
 	}
 	if errors.As(e.err, &priceError) {
 		*e.status = http.StatusBadRequest
-		e.exception.Message = priceError.Message
-		e.exception.Data = priceError.Ctx
+		response.SetExceptionPayload(e.exception, priceError.CustomException)
 	}
 	if errors.As(e.err, &productNameError) {
 		*e.status = http.StatusNotFound
-		e.exception.Message = productNameError.Message
-		e.exception.Data = productNameError.Ctx
+		response.SetExceptionPayload(e.exception, productNameError.CustomException)
 	}
 	if errors.As(e.err, &productIDError) {
 		*e.status = http.StatusNotFound
-		e.exception.Message = productIDError.Message
-		e.exception.Data = productIDError.Ctx
+		response.SetExceptionPayload(e.exception, productIDError.CustomException)
 	}
 }
