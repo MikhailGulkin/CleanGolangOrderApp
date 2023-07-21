ca7b24af33315aa6172d3ba4a1255457c3e8f954
Bol4onok <what@about.com>
2023-07-21 11:38:34 +0300

    upd

--- internal/infrastructure/db/dao/order/saga.go
+++ internal/infrastructure/db/dao/order/saga.go
@@ -18,7 +18,6 @@ func (dao *SagaDAOImpl) CheckSagaCompletion(orderID uuid.UUID) (bool, error) {
 	var count int64
 	fmt.Println(orderID)
 	res := dao.Session.Model(&models.Order{}).Where("saga_status = ? AND id = ?", "Pending", orderID).Count(&count)
-	fmt.Println(count, res.Error)
 	return count > 0, res.Error
 }
 func (dao *SagaDAOImpl) UpdateOrderSagaStatus(orderID uuid.UUID, status string, tx interface{}) error {
